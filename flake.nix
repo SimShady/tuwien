85c72186caace9a380920679ba98d13701f2f560
Simon Babovic <simon@babovic.at>
2025-05-15 13:25:35 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "TU Wien Educational Programs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          gcc
+          gnuplot
+          fftw
+          eigen
+        ];
+      };
+    });
+}
